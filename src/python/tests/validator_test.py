2a3e8bd7739f61fab740801ff3af55d350070f86
Vigneshkna <Vigneshkanna98@gmail.com>
2025-04-25 00:14:21 +0530

    feat(test):added testcasec for python

--- src/python/tests/validator_test.py
+++ src/python/tests/validator_test.py
@@ -1,6 +1,11 @@
+import argparse
 import pytest
 from how2validate.validator import validate_email, parse_arguments, validate, main
 from how2validate.utility.interface.validationResult import ValidationResult
+from how2validate.utility.tool_utility import get_secretprovider, get_secretservices, validate_choice, get_current_timestamp
+
+#Mock the current timestamp
+current_timestamp = get_current_timestamp()
 
 def test_validate_email_valid():
     """Test validate_email with a valid email."""
@@ -150,3 +155,71 @@ def test_main_with_secretscope(mocker):
 
     # Assertions
     mock_get_secretscope.assert_called_once()
+
+def test_with_provider_services(mocker):
+    """Test with valid provider and service."""
+    
+    # Mock the requests.post response
+    mock_response = mocker.MagicMock()
+    mock_response.status_code = 200
+    mock_response.text = '{"status": "success"}'
+    mocker.patch("requests.post", return_value=mock_response)
+
+    # Mock handle_active_status
+    mock_provider_service_response = mocker.MagicMock()
+    mock_provider_service_response.status = 200
+    mock_provider_service_response.app = "How2Validate"
+    mock_provider_service_response.data.validate.state = "Active"
+    mock_provider_service_response.data.validate.message = "The provided secret 'anthropic_api_key' is currently active and operational."
+    mock_provider_service_response.data.validate.report = "email@how2validate.com"
+    mock_provider_service_response.data.provider = "anthropic"
+    mock_provider_service_response.data.services = "anthropic_api_key"
+    mock_provider_service_response.timestamp = current_timestamp
+    mocker.patch("how2validate.utility.tool_utility.handle_active_status", return_value=mock_provider_service_response)
+
+    # Call the function
+    result = validate(
+        provider="anthropic",
+        service="anthropic_api_key",
+        secret="valid_key",
+        response=False,
+        report=None,
+        isBrowser=True
+    )
+
+    # Assertions
+    assert result 
+
+def test_with_invalid_provider(mocker):
+    """Test validate_choice with an invalid service name."""
+    
+    # Call the function with an invalid service
+    invalid_provider = "invalid_service"
+    valid_provider = get_secretprovider()
+
+    # Mock get_secretservices to return a list of valid provider
+    mocker.patch("how2validate.utility.tool_utility.get_secretprovider", return_value=valid_provider)
+
+    # Assertions
+    with pytest.raises(argparse.ArgumentTypeError) as excinfo:
+        validate_choice(invalid_provider, valid_provider)
+    
+    # Check the exception message
+    assert str(excinfo.value) == f"Invalid choice: '{invalid_provider}'. Choose from {', '.join(valid_provider)}."
+
+def test_with_invalid_services(mocker):
+    """Test validate_choice with an invalid service name."""
+    
+    # Call the function with an invalid service
+    invalid_service = "invalid_service"
+    valid_services = get_secretservices()
+
+    # Mock get_secretservices to return a list of valid services
+    mocker.patch("how2validate.utility.tool_utility.get_secretservices", return_value=valid_services)
+
+    # Assertions
+    with pytest.raises(argparse.ArgumentTypeError) as excinfo:
+        validate_choice(invalid_service, valid_services)
+    
+    # Check the exception message
+    assert str(excinfo.value) == f"Invalid choice: '{invalid_service}'. Choose from {', '.join(valid_services)}."
