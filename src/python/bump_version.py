295bd9828cd4f1b7347b6e577f7b72b00ecb0b97
Vigneshkna <Vigneshkanna98@gmail.com>
2025-09-21 22:56:52 +0530

    fix: Update CONFIG_FILE path to ensure correct loading of Config.ini

--- src/python/bump_version.py
+++ src/python/bump_version.py
@@ -34,7 +34,7 @@ import requests
 import sys
 
 # Constants
-CONFIG_FILE = "Config.ini"
+CONFIG_FILE = os.path.join(os.path.dirname(__file__), "Config.ini")
 PACKAGE_NAME = "how2validate"  # Change this if your PyPI package name differs
 
 def load_config():
